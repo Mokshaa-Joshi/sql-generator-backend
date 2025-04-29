62145d9e36eacbb285e0d9ec66a2f8065a06673d
Mokshaa-Joshi <122172274+Mokshaa-Joshi@users.noreply.github.com>
2025-04-29 12:09:48 +0530

    Update api.py

--- app/api.py
+++ app/api.py
@@ -1,12 +1,12 @@
 from fastapi import APIRouter
 from pydantic import BaseModel
 from app.services import generate_sql
+from app.models import QueryRequest, QueryResponse
 
 router = APIRouter()
 
-class QueryRequest(BaseModel):
-    prompt: str
 
-@router.post("/generate-sql")
+@router.post("/generate-sql",response_model=QueryResponse)
 def generate_sql_query(req: QueryRequest):
+    sql_query = generate_sql(req.prompt)
     return {"sql": generate_sql(req.prompt)}
